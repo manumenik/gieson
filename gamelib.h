9c1fb174a4e51e369c88a71e96e9c036612a487d
manumenik <33425662+manumenik@users.noreply.github.com>
2018-01-19 09:57:38 +0100

    Update gamelib.h

--- gamelib.h
+++ gamelib.h
@@ -1,3 +1,28 @@
-/* Questo file contiene le dichiarazioni delle funzioni crea_mappa(), gioca(), termina_gioco().
- Continene anche le definizioni dei tipi utilizzati in gamelib.c:
- struct Giocatore, struct Zona, enum Stato_giocatore, enum Tipo_zona, enum Tipo_oggetto. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//IN QUESTO FILE.H VENGONO DICHIARATI GLI STRUCT CONTENENTI LE VARIABILI DI ZONA E GIOCATORE E I typedef DELL STATO GIOCATORE
+//DELLE ZONE E DEGLI OGGETTI
+
+typedef enum {Morto, Ferito, Vivo}Stato_Giocatore;
+typedef enum {Cucina, Soggiorno, Rimessa, Strada, Lungo_Lago, Uscita_Campeggio}Tipo_Zona;
+typedef enum {Cianfrusaglia, Bende, Coltello, Pistola, Benzina, Chiavi_Auto, Adrenalina}Tipo_Oggetto;
+
+typedef struct Zona
+{
+	int i_Zona;
+	int Oggetto_Nascosto;
+	Tipo_Zona N_Zona;
+	Tipo_Oggetto Oggetto;
+	struct Zona *Zona_Successiva;
+}Zona;
+
+typedef struct Giocatore
+{
+	int Vittoria;
+	struct Zona *Posizione;
+	Stato_Giocatore Stato;
+	unsigned short Zaino[7];
+	int Mossa;
+}Giocatore;
